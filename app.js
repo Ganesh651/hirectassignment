d8b25b48ea28dcbbd5ce74ca913f1fcaee72ce77
Ganesh651 <ganireddy651@gmail.com>
2023-10-30 17:52:22 +0530

    initial set up and register,login API's

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const bcrypt = require("bcrypt");
+const cors = require("cors")
 const jwt = require("jsonwebtoken");
 
 const {open} = require("sqlite");
@@ -8,6 +9,7 @@ const sqlite3 = require("sqlite3");
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const dbPath = path.join(__dirname, "products.db")
 let db = null 
